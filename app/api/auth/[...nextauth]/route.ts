184f3694ebe6f8e31faf8596f5426218712c128b
DirkSchlossmacher <62424946+DirkSchlossmacher@users.noreply.github.com>
2023-10-04 16:23:48 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
 import NextAuth from "next-auth/next"
-import AzureADProvider from "next-auth/providers/azure-ad"
 import { config } from "auth"
 
 const handler = NextAuth(config)
 export { handler as GET, handler as POST }
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.AZURE_AD_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
-      tenantId: process.env.AZURE_AD_TENANT_ID,
-    }),
-    // ...add more providers here
-  ],
-}
-export default NextAuth(authOptions)
